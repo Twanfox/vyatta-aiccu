44a707b81c8ee99a4ad8fd6b487c3fd428e47186
Twan Fox <twanfox@gmail.com>
2015-10-28 16:18:28 -0500

    Source alterations to support VyOS 1.1.x series fork of Vyatta  - Removed unneeded firewall configuration options (works with inline firewall nodes).  - Added defaultroute option to tunnel configurations  - Updated script to include defaultroute when generating aiccu.conf  - Added operational commands to show aiccu interfaces on 'show interfaces'

--- install.sh
+++ install.sh
@@ -6,8 +6,14 @@ SBINDIR=/opt/vyatta/sbin
 mkdir -p "${DESTDIR}${DATADIR}/vyatta-cfg/templates"
 cp -ar templates-cfg/* "${DESTDIR}${DATADIR}/vyatta-cfg/templates"
 
+mkdir -p "${DESTDIR}${DATADIR}/vyatta-op/templates"
+cp -ar templates-op/* "${DESTDIR}${DATADIR}/vyatta-op/templates"
+
 mkdir -p "${DESTDIR}${SBINDIR}"
 cp -a scripts/vyatta-update-aiccu "${DESTDIR}${SBINDIR}"
 
 mkdir -p "${DESTDIR}/etc/insserv/overrides"
 cp -ar debian/insserv.overrides.aiccu "${DESTDIR}/etc/insserv/overrides"
+
+mkdir -p "${DESTDIR}/etc/netdevice.d"
+cp -ar etc/netdevice.d/aiccu "${DESTDIR}/etc/netdevice.d"
